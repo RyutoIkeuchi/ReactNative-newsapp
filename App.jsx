355a873b4f823b6c459a117b17a11dc5263b5e78
ryuto <ryu.i7133@gmail.com>
2021-05-09 15:54:51 +0900

    apiwを叩いてnewsを表示

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import DetailScreen from './screens/DetailScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen name="Home" component={HomeScreen} />
+				<Stack.Screen name="Detail" component={DetailScreen} />
+			</Stack.Navigator>
+			<StatusBar style="auto" />
+		</NavigationContainer>
+	);
+}
